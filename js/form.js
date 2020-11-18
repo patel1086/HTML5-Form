744e605f9548a828b7b86cb4d683413daf172c8c
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-11-18 21:26:04 +0530

    [Jitendra]Added: Password Validation in Form

--- js/form.js
+++ js/form.js
@@ -14,19 +14,29 @@ const emailError = document.querySelector('.email-error');
 email.addEventListener('input',function(){
     let emailRegex = RegExp('^[a-zA-Z0-9+_-]+([.][a-zA-Z0-9]+)*@([a-zA-Z0-9]+)([.][a-z]+)?[.][a-z]{2,}$');
     if(emailRegex.test(email.value))
-    emailError.textContent="";
+        emailError.textContent="";
     else emailError.textContent="Email Id is Invalid";
 });
 
 //Mobile Number Validation
-const mobile = document.querySelector('#mobile');
+const mobile = document.querySelector('#tel');
 const mobError = document.querySelector('.mobile-error');
 mobile.addEventListener('input',function(){
-let mobileRegex = RegExp('[91]{2}[7-9]{1}[0-9]{9}');
-if(mobileRegex.test(mobile.value))
-mobError.textContent="";
-else mobError.textContent="Invalid Mobile number";
-});
+    let mobileRegex = RegExp('^[0-9]{2}(\s{1}[0-9]{10})$');
+    if(mobileRegex.test(mobile.value))
+        mobError.textContent="";
+    else mobError.textContent="Invalid Mobile number";
+    });
+
+//Password Validation
+const pwd = document.querySelector('#pwd');
+const pwdError = document.querySelector('.pwd-error');
+pwd.addEventListener('input',function(){
+    let pwdRegex = RegExp('^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#!@%&])[a-zA-Z0-9].{8,}$');
+    if(pwdRegex.test(pwd.value))
+        pwdError.textContent="";
+    else pwdError.textContent="Invalid Password";
+    });
 
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
