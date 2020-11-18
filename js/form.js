0eac59ed5a70ee6e8b4880a345286d05938273ed
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-11-18 17:38:18 +0530

    [Jitendra]Added: Email Validation in Form

--- js/form.js
+++ js/form.js
@@ -1,3 +1,4 @@
+//Name Validation
 const text = document.querySelector('#text');
 const textError = document.querySelector('.text-error');
 text.addEventListener('input', function(){
@@ -5,6 +6,16 @@ text.addEventListener('input', function(){
     if(nameRegex.test(text.value))
         textError.textContent="";
     else textError.textContent="Name is Incorrect";
+});
+
+//Email Validation
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-error');
+email.addEventListener('input',function(){
+    let emailRegex = RegExp('^[a-zA-Z0-9+_-]+([.][a-zA-Z0-9]+)*@([a-zA-Z0-9]+)([.][a-z]+)?[.][a-z]{2,}$');
+    if(emailRegex.test(email.value))
+    emailError.textContent="";
+    else emailError.textContent="Email Id is Invalid";
 });
 
 
